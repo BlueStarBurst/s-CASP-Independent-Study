3cbd6e624422243bc04ef54799adef19684dc695
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2024-03-04 16:19:23 -0600

    mod files

---
+++ modinfo.lua
@@ -0,0 +1,29 @@
+--The name of the mod displayed in the 'mods' screen.
+name = "sCASP reader"
+
+--A description of the mod.
+description = "A mod that reads the current state of the game and outputs it to a file. This is a mod for the sCASP project."
+
+--Who wrote this awesome mod?
+author = "gamers"
+
+--A version number so you can ask people if they are running an old version of your mod.
+version = "1.0"
+
+--This lets other players know if your mod is out of date.  This typically needs to be updated every time there's a new game update.
+api_version = 6
+
+-- Compatible with both the base game and reign of giants
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+
+--Some mods may crash or not work correctly until the game is restarted after the mod is enabled/disabled
+restart_required = false
+
+-- Set this to true to prevent _ANY_ other mods from loading while this mod is enabled.
+standalone = false
+
+icon_atlas = "screechericon.xml"
+icon = "screechericon.tex"
+
+forumthread = ""
---
+++ modmain.lua
@@ -0,0 +1,41 @@
+-- A Don't Starve mod that reads the current state of the game and outputs it to a file. This is a mod for the sCASP project.
+local require = GLOBAL.require
+
+require "constants"
+
+print("sCASP reader modmain.lua loaded")
+
+-- print player coordinates to logs
+
+function PreparePlayerCharacter(player)
+    -- The player entity isn't quite ready to equip items yet, so wait one frame
+    player:DoTaskInTime(0, function()
+
+        -- Grab a reference to the home campfire
+        local x, y, z = player.Transform:GetWorldPosition()
+
+		print("Player position STARTREADER: ", x, y, z)
+
+        -- local ents = TheSim:FindEntities(x, y, z, 20, {"campfire"})
+        -- player.homefirepit = ents[1]
+
+        -- -- Make sure the player is facing it
+        -- player.Transform:SetRotation(player:GetAngleToPoint(Point(player.homefirepit.Transform:GetWorldPosition())))
+    end)
+
+    player:DoPeriodicTask(1, function()
+        local playerfacing = player.Transform:GetRotation()
+		local x, y, z = player.Transform:GetWorldPosition()
+		print("Player position: ", x, y, z, "\nPlayer facing: ", playerfacing)
+        -- GLOBAL.TheCamera:SetHeadingTarget(-playerfacing + 180)
+    end)
+
+    -- debugging help
+    -- GLOBAL.require("consolecommands")
+    -- GLOBAL.c_select(player)
+end
+AddSimPostInit(PreparePlayerCharacter)
+
+
+
+
