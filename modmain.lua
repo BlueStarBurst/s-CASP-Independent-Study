787e50770d05bb928be485b29302c118530f8aa9
bluestarburst <hargreaves.bryant@gmail.com>
2024-03-23 11:49:53 -0500

    lua

--- modmain.lua
+++ modmain.lua
@@ -10,6 +10,8 @@
 -- A Don't Starve mod that reads the current state of the game and outputs it to a file. This is a mod for the sCASP project.
 local require = GLOBAL.require
 
+local Text = require "widgets/text"
+
 require "constants"
 
 print("sCASP reader modmain.lua loaded")
@@ -17,8 +19,16 @@ print("sCASP reader modmain.lua loaded")
 -- print player coordinates to logs
 
 function PreparePlayerCharacter(player)
+    local numbertext = Text(GLOBAL.BUTTONFONT, 24, "Test") --Make some text
+    numbertext:SetColour(1,1,1,1) --Set the colour to white
     -- The player entity isn't quite ready to equip items yet, so wait one frame
     player:DoTaskInTime(0, function()
+
+        
+
+        numbertext:SetPosition(300,100) --Put text somewhere (starts at bottom left)
+
+        
 
         -- Grab a reference to the home campfire
         local x, y, z = player.Transform:GetWorldPosition()
@@ -36,6 +46,7 @@ function PreparePlayerCharacter(player)
         local playerfacing = player.Transform:GetRotation()
 		local x, y, z = player.Transform:GetWorldPosition()
 		print("Player position: ", x, y, z, "\nPlayer facing: ", playerfacing)
+        numbertext:SetString( "Player position:   " .. x .. "   " .. y .. "   " .. z ) --Change the text on the fly
         -- GLOBAL.TheCamera:SetHeadingTarget(-playerfacing + 180)
     end)
 
