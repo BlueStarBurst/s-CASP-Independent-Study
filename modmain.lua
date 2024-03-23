b7d0229f30712ff79868eb3ff6b166461132f69f
bluestarburst <hargreaves.bryant@gmail.com>
2024-03-23 13:03:55 -0500

    infinite server and send/receive functions

--- modmain.lua
+++ modmain.lua
@@ -22,9 +22,21 @@ print("sCASP reader modmain.lua loaded")
 
 -- print player coordinates to logs
 
+function SendData(data)
+    print("Sending data to server")
+    tcp:send(data)
+end
+
+function ReceiveData()
+    print("Receiving data from server")
+    local s, status, partial = tcp:receive()
+    -- print("Server:", s or partial)
+    return s or partial
+end
+
 function PreparePlayerCharacter(player)
-    local numbertext = Text(GLOBAL.BUTTONFONT, 24, "Test") --Make some text
-    numbertext:SetColour(1,1,1,1) --Set the colour to white
+    -- local numbertext = Text(GLOBAL.BUTTONFONT, 24, "Test") --Make some text
+    -- numbertext:SetColour(1,1,1,1) --Set the colour to white
     -- The player entity isn't quite ready to equip items yet, so wait one frame
     player:DoTaskInTime(0, function()
 
@@ -33,41 +45,25 @@ function PreparePlayerCharacter(player)
         tcp:connect(host, port);
         tcp:settimeout(0.1)
 
-        tcp:send("Player position:   " .. 0 .. "   " .. 0 .. "   " .. 0 .. "\n")
-
-        numbertext:SetPosition(300,100) --Put text somewhere (starts at bottom left)
-
-        
-
         -- Grab a reference to the home campfire
         local x, y, z = player.Transform:GetWorldPosition()
 
 		print("Player position STARTREADER: ", x, y, z)
 
-        
-
-        -- local ents = TheSim:FindEntities(x, y, z, 20, {"campfire"})
-        -- player.homefirepit = ents[1]
-
-        -- -- Make sure the player is facing it
-        -- player.Transform:SetRotation(player:GetAngleToPoint(Point(player.homefirepit.Transform:GetWorldPosition())))
     end)
 
     player:DoPeriodicTask(1, function()
         local playerfacing = player.Transform:GetRotation()
 		local x, y, z = player.Transform:GetWorldPosition()
-		print("Player position: ", x, y, z, "\nPlayer facing: ", playerfacing)
-        numbertext:SetString( "Player position:   " .. x .. "   " .. y .. "   " .. z ) --Change the text on the fly
-        -- GLOBAL.TheCamera:SetHeadingTarget(-playerfacing + 180)
 
         -- send to position to the server
         print("Sending player position to server")
-        tcp:send("Player position:   " .. x .. "   " .. y .. "   " .. z .. "\n")
-        
+        SendData("Player position:   " .. x .. "   " .. y .. "   " .. z .. "\n")        
         
         print("Receiving from server")
-        local s, status, partial = tcp:receive()
-        print("Server:", s or partial)
+
+        print(ReceiveData())
+        
         print("\n\n\n")
         
 
