64b47afef0b79829216e7fe2e83583a81c40c481
Dan Nguyen <ndan0112@gmail.com>
2024-05-20 17:44:41 -0500

    Revert "Merge branch 'main' into test"
    This reverts commit 2e8a5e71cc436bd48c879fa86bb53cb67f1c9675, reversing
    changes made to b8c257191477a6ec5864176a823d38a21ea02418.

--- agents/server.py
+++ agents/server.py
@@ -1,29 +1,50 @@
-from TCP import CommTCP
+import sys
+import signal
+import socket
 import json
 from perception import get_action
 
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 12345  # Port to listen on (non-privileged ports are > 1023)
 
 print("Server started")
-CommTCP.connect()  
+
+# print all the ip addresses of the server
+print("IP addresses of the server:", socket.gethostbyname_ex(socket.gethostname()))
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    def signal_handler(sig, frame):
+        s.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
     
-while True:
-    isData, data = CommTCP.receive()
-    if not isData:
-        continue  
-            
-    with open("data.json", "w") as f:
-        f.write(data)
-                        
-    if not data:
-        break
-    
-    # send json data of action: "chop" to the client
-    desc, func, args = get_action(data)
-    
-    data = json.dumps({"desc": desc, "func": func, "args": args})
-    # encode to bytes
-    data = data.encode("utf-8")
-    CommTCP.send(data)
-            
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024*10000)
+                data = data.decode("utf-8") # decode the data from json
+                
+                print("Received data:", data)
+                
+                with open("data.json", "w") as f:
+                    f.write(data)
+                                    
+                if not data:
+                    break
+                
+                # send json data of action: "chop" to the client
+                desc, func, args = get_action(data)
+                
+                data = json.dumps({"desc": desc, "func": func, "args": args})
+                # encode to bytes
+                data = data.encode("utf-8")
+                
+                conn.sendall(data)
                 
                 
