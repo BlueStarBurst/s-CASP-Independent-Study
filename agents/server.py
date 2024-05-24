6b50517f3699a1a811631f3d2b0e8082de546a19
Dan Nguyen <ndan0112@gmail.com>
2024-05-24 14:25:41 -0500

    Working FastAPI server call from POSTMAN

--- agents/server.py
+++ agents/server.py
@@ -1,50 +1,41 @@
-import sys
-import signal
-import socket
-import json
 from perception import get_action
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 12345  # Port to listen on (non-privileged ports are > 1023)
+PORT = 48782  # Port to listen on (non-privileged ports are > 1023)
 
-print("Server started")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
-# print all the ip addresses of the server
-print("IP addresses of the server:", socket.gethostbyname_ex(socket.gethostname()))
+class Data(BaseModel):
+    isInLight: bool
+    sanity: str
+    hunger: str
+    health: str
+    inventory: list
+    equipped: list
+    entitiesOnScreen: list
+    timeOfDay: dict
+    season: str
+    biome: str
 
+app = FastAPI(debug=True)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    def signal_handler(sig, frame):
-        s.close()
-        sys.exit(0)
+#Set up cors to allow all localhost origins requests, with LUA
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    signal.signal(signal.SIGINT, signal_handler)
+@app.post("/action_from_perception/")
+async def get_action(data: Data):
+    return data
     
-    while True:
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024*10000)
-                data = data.decode("utf-8") # decode the data from json
-                
-                print("Received data:", data)
-                
-                with open("data.json", "w") as f:
-                    f.write(data)
-                                    
-                if not data:
-                    break
-                
-                # send json data of action: "chop" to the client
-                desc, func, args = get_action(data)
-                
-                data = json.dumps({"desc": desc, "func": func, "args": args})
-                # encode to bytes
-                data = data.encode("utf-8")
-                
-                conn.sendall(data)
-                
-                
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("server:app", host=HOST, port=PORT, reload=True)
+    
+
