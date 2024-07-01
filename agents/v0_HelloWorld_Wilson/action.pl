b32032a888a1d94e6a45d4692053d838b57991d7
Dan Nguyen <ndan0112@gmail.com>
2024-06-30 20:16:57 -0500

    Working so far. Need fixed

--- agents/v0_HelloWorld_Wilson/action.pl
+++ agents/v0_HelloWorld_Wilson/action.pl
@@ -1,28 +1,27 @@
-pickable_check(GUID) :- item_on_screen(ITEM, GUID), ITEM\=berrybush, ITEM\=sapling.
-pickable_check(GUID) :- item_on_screen(ITEM, GUID), pickable(GUID), ITEM=berrybush, ITEM=sapling.
 
+good_item(carrot).
+good_item(axe).
+good_item(flint).
+good_item(twigs).
+good_item(log).
+good_item(cutgrass).
+good_item(berries).
+good_item(sapling).
 
-good_pick(planted_carrot).
-good_pick(carrot).
-good_pick(axe).
-good_pick(twigs).
-good_pick(flint).
-good_pick(log).
-good_pick(cutgrass).
-good_pick(berries).
-good_pick(berrybush).
-good_pick(sapling).
+collectable(GUID) :- pickable(GUID).
+good_pick(GUID) :- collectable(GUID), item_on_screen(X, GUID), good_item(X), -item_in_inventory(X, N).
+good_pick(GUID) :- collectable(GUID), item_on_screen(X, GUID), good_item(X), item_in_inventory(X, N), N.<.8.
 
+torch_ingredients:- item_in_inventory(cutgrass, X), X.>=.2, item_in_inventory(twigs, Y), Y.>=.2.
 
-torch_ingredients(A):- item_in_inventory(cutgrass, X), item_in_inventory(twigs, X), X.>=.2.
+axe_ingredients:- item_in_inventory(flint, X), X.>=.1, item_in_inventory(twigs, Y), Y.>=.1.
 
-axe_ingredients(A):- item_in_inventory(flint, X), item_in_inventory(twigs, X), X.>=.1.
+campfire_ingredients:- item_in_inventory(log, X), X.>=.2, item_in_inventory(cutgrass, Y), Y.>=.3.
 
-campfire_ingredients(A):- item_in_inventory(log, X), X.>=.2, item_in_inventory(cutgrass, Y), Y.>=.3.
-
-garland_ingredients(A):- item_in_inventory(petals, X), X.>=.12.
+garland_ingredients:- item_in_inventory(petals, X), X.>=.12.
 
 only_time(A) :- time(A, T), T=early, T=mid, T=end.
+
 
 % action(short_description, functionToUseInLua, FunctionArguments)
 action(equip_torch_night_hostile, equip, GUID) :- only_time(night), not equipment(torch), item_in_inventory(torch, X), hostile(E), slot_in_inventory(torch, GUID).
@@ -35,12 +34,12 @@ action(build_campfire, build, campfire) :- time(dusk, end), campfire_ingredients
 action(equip_torch_night_no_campfire, equip, GUID) :- only_time(night), item_in_inventory(torch, X), not equipment(torch), slot_in_inventory(torch, GUID).
 action(build_torch_night, build, torch) :- time(night), torch_ingredients(X), not item_in_inventory(torch, X).
 action(cook_food, cook, GUID) :- cookable(GUID), slot_in_inventory(X, GUID), time(night).
-action(build_axe, build, axe) :- axe_ingredients(X), not equipment(axe), not item_in_inventory(axe, N).
+action(build_axe, build, axe) :- axe_ingredients, not equipment(axe), not item_in_inventory(axe, N).
 action(build_torch, build, torch) :- torch_ingredients(X), not equipment(torch), not item_in_inventory(torch, N).
 action(equip_axe, equip, axe) :- not equipment(axe), item_in_inventory(axe, N), -only_time(night).
 action(pick_log, pick_entity, GUID) :- item_on_screen(log, GUID).
 action(chop_tree, chop_tree, GUID) :- item_on_screen(X, GUID), choppable(X), equipment(axe).
-action(pick_anything, pick_entity, GUID) :- item_on_screen(X, GUID), good_pick(X), pickable_check(GUID).
+action(pick_anything, pick_entity, GUID) :- item_on_screen(X, GUID), good_pick(GUID).
 action(wander, wander, nil) :- not only_time(night).
 
-% ?- action(DESC, FUNC, ARGS).
+?- good_pick(GUID) .
